9926228e9f65e1c6b9314794255c4bd7999265f8
Ben Metzger <benjpac@gmail.com>
2017-04-28 10:54:42 -0700

    add form for entering word

--- app.rb
+++ app.rb
@@ -5,4 +5,15 @@ require('./lib/word')
 require('pry')
 also_reload('lib/**/*.rb')
 
-get
+get('/') do
+  @words = Word.all()
+  erb(:index)
+end
+
+post('/') do
+  word = params[:word]
+  new_word = Word.new({:word=> word})
+  new_word.save()
+  @words = Word.all()
+  erb(:index)
+end
